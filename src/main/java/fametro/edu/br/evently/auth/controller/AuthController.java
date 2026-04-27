6d6beab70b81d774d438e56e152f9815aed7c197
MIGUEL-ARCANJO-BRASIL <devmiguelarcanjo@gmail.com>
2026-04-27 12:11:34 -0400

    fix: redirect login and save context user

--- src/main/java/fametro/edu/br/evently/auth/controller/AuthController.java
+++ src/main/java/fametro/edu/br/evently/auth/controller/AuthController.java
@@ -2,16 +2,21 @@ package fametro.edu.br.evently.auth.controller;
 
 import fametro.edu.br.evently.auth.dto.UserRegisterDTO;
 import fametro.edu.br.evently.auth.service.AuthService;
-import fametro.edu.br.evently.user.model.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/auth")
@@ -19,6 +24,7 @@ import java.util.Optional;
 public class AuthController {
 
     private final AuthService authService;
+    private final AuthenticationManager authenticationManager;
 
     @GetMapping("/login")
     public String login(@RequestParam(value = "erro", required = false) String erro,
@@ -39,29 +45,41 @@ public class AuthController {
         return "auth/register";
     }
 
+
     @PostMapping("/register")
     public String register(@Valid @ModelAttribute("usuarioForm") UserRegisterDTO form,
                            BindingResult result,
                            Model model,
-                           RedirectAttributes redirectAttributes) {
+                           HttpServletRequest request,
+                           HttpServletResponse response) {
 
         if (result.hasErrors()) {
             return "auth/register";
         }
 
-        if (authService.existsByEmail(form.getEmail())) {
-            model.addAttribute("erro", "Este e-mail já está cadastrado.");
-            return "auth/events";
-        }
+        try {
+            authService.register(form);
 
-        if (authService.existsByCpf(form.getCpf())) {
-            model.addAttribute("erro", "Este CPF já está cadastrado.");
+            UsernamePasswordAuthenticationToken authRequest =
+                    UsernamePasswordAuthenticationToken.unauthenticated(
+                            form.getEmail(),
+                            form.getPassword()
+                    );
+
+            Authentication authentication = authenticationManager.authenticate(authRequest);
+
+            SecurityContext context = SecurityContextHolder.createEmptyContext();
+            context.setAuthentication(authentication);
+            SecurityContextHolder.setContext(context);
+
+            HttpSessionSecurityContextRepository securityContextRepository =
+                    new HttpSessionSecurityContextRepository();
+            securityContextRepository.saveContext(context, request, response);
+
+            return "redirect:/events";
+        } catch (RuntimeException e) {
+            model.addAttribute("erro", e.getMessage());
             return "auth/register";
         }
-
-        authService.register(form);
-        redirectAttributes.addFlashAttribute("sucesso", "Conta criada! Faça login.");
-        return "redirect:/home";
     }
-
 }
--- src/main/java/fametro/edu/br/evently/security/SecurityConfig.java
+++ src/main/java/fametro/edu/br/evently/security/SecurityConfig.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -60,6 +62,10 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
